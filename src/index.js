4ebc7b3d23ec738054a420823fbdae8d9d19d6de
Ricardo <ricoledan@gmail.com>
2020-08-05 11:51:19 -0400

    add text for tv shows out of bounds of the api

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 const { getImdbId, getNextEpisode } = require('imdb-api-client')
 
 module.exports = async function App(context) {
-  let metaInfo = {}
-
   if (context.event.isText) {
     const getId = await getImdbId(context.event.text.toLowerCase())
     const getNextEpisodeInfo = await getNextEpisode(getId)
-    const showEpisodeInfo = `Next episode of ${getNextEpisodeInfo.name} "${getNextEpisodeInfo.nextEpisodeTitle}" aires ${getNextEpisodeInfo.releaseDate}`
 
-    await context.sendText(showEpisodeInfo)
+    if (getNextEpisodeInfo) {
+      const showEpisodeInfo = `Next episode of ${getNextEpisodeInfo.name} "${getNextEpisodeInfo.nextEpisodeTitle}" aires ${getNextEpisodeInfo.releaseDate}`
+      await context.sendText(showEpisodeInfo)
+    } else {
+      await context.sendText(
+        'We do not have any information on the tv series you requested'
+      )
+    }
   }
 }
